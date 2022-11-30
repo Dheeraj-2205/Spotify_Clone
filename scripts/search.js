7d525969665786e5849b4d76c08b12c43cd74258
faran <faranmd1@gmail.com>
2022-11-30 20:53:16 +0530

    make some changes

--- scripts/search.js
+++ scripts/search.js
@@ -2,14 +2,25 @@ let openCateg = (e) =>{
     // event.preventDefault();
     // let categoryId = event.target.parentElement.parentElement.id;
     // console.log(event.target);
-    console.log(e.target.id);
-    // localStorage.setItem("catID", categoryId);
+        console.log(e);
+    localStorage.setItem("catID", e);
 
 }
 
 let categories = document.querySelectorAll(".categories-container");
 
-for(var i=0; i<categories.length; i++){
-    categories[i].addEventListener("click", openCateg);
-}
+// for(var i=0; i<categories.length; i++){
+//     // console.log(categories[i].id);
+//     var category = categories[i];
+//     categories[i].addEventListener("click", ()=>{
+//         console.log(category.id);
+        
+//     });
+// }
+
+categories.forEach(e=>{
+    e.onclick = () =>{
+        openCateg(e.id);
+    }
+});
 
